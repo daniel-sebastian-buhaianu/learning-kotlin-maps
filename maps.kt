b8e8d925db823dfe3ae8e0276ee2a8fc12335fb4
Daniel-Sebastian Buhaianu <dsb99.dev@gmail.com>
2023-04-12 10:09:00 +0300

    learning about maps in kotlin

---
+++ maps.kt
@@ -0,0 +1,32 @@
+// A Map is a collection consisting of keys and values. 
+// It's called a map because unique keys are mapped to other values. 
+// A key and its accompanying value are often called a key-value pair.
+
+fun main() {
+
+    val solarSystem = mutableMapOf(
+        "Mercury" to 0,
+        "Venus" to 0,
+        "Earth" to 1,
+        "Mars" to 2,
+        "Jupiter" to 79,
+        "Saturn" to 82,
+        "Uranus" to 27,
+        "Neptune" to 14
+    )
+
+    println(solarSystem.size)
+    
+    solarSystem["Pluto"] = 5
+    println(solarSystem.size)
+    println(solarSystem["Pluto"])
+
+    println(solarSystem["Theia"])
+
+    solarSystem.remove("Pluto")
+    println(solarSystem.size)
+
+    solarSystem["Jupiter"] = 78
+    println(solarSystem["Jupiter"])
+
+}
